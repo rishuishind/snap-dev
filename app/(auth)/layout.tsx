6e4639af6fa0db6c7fa6e4d19fc1683e60043862
rishabh <hespnod2348@gmai.com>
2024-02-09 17:48:53 +0530

    made the login page layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const authLayout=({children}:{children:React.ReactNode})=>{
+    return <div className="bg-slate-600">{children}</div>
+}
+
+export default authLayout;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
 			<div className="min-h-screen flex flex-col items-center justify-center max-w-7xl mx-auto">
 				<Navbar />
 				<main className='flex flex-1 flex-col md:flex-row items-center justify-center px-8 mt-4'>
-					<div className='flex-1 md:text-left text-center h-full'>
-						<h1 className='text-4xl md:text-6xl font-bold'>SnapNext 4 programmers!</h1>
+					<div className='flex-1  md:text-left text-center h-full'>
+						<h1 className='text-4xl md:text-6xl font-bold'>Namastey Developers!</h1>
 						<p className='mt-4 text-xl font-semibold'>
 							Share your code with your friends to get feedback and improve your code.
 						</p>
@@ -22,7 +22,7 @@ export default function Home() {
 								asChild
 								className='mt-4 bg-black text-white flex items-center rounded-lg gap-2 mx-auto md:mx-0'
 							>
-								<Link href={"/login"} className='max-w-max'>
+								<Link href="/login" className='max-w-max'>
 									<Image src='/logo.svg' width={20} height={20} alt='Snapchat logo' />
 									Log in to explore
 								</Link>
